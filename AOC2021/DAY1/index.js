245e746847f6ba3a64bba04a02485949e47accff
luke <luc.dewit@hotmail.nl>
2021-12-01 06:28:10 +0100

    day 1 code

--- AOC2021/DAY1/index.js
+++ AOC2021/DAY1/index.js
@@ -4,12 +4,36 @@ const input = fs.readFileSync('./input.txt')
                 .toString()
 
 const part1 = () => {
-    return
+    const depths = input.split`\n`.map(Number);
+
+    let n = 0;
+
+    for (let i = 1; i < depths.length; i++) {
+        const prevDepth = depths[i - 1];
+        const depth = depths[i];
+
+        console.log(depth, prevDepth, depth > prevDepth)
+
+        n += depth > prevDepth;
+    }
+
+    return n
 }
 
 const part2 = () => {
-    return
+    const depths = input.split`\n`.map(Number);
+
+    let n = 0;
+
+    for (let i = 2; i < depths.length - 1; i++) {
+        const prevSum = depths[i - 2] + depths[i - 1] + depths[i];
+        const sum = depths[i - 1] + depths[i] + depths[i + 1];
+
+        n += sum > prevSum;
+    }
+
+    return n
 }
 
-console.log(part1())
-// console.log(part2())
+// console.log(part1())
+console.log(part2())
