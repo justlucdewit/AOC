05fda89eda3708046c641f5eb9928ed0e7b3ebf0
luke <luc.dewit@hotmail.nl>
2021-12-02 06:19:53 +0100

    day 2 code

--- AOC2021/DAY2/index.js
+++ AOC2021/DAY2/index.js
@@ -2,9 +2,30 @@ const fs = require('fs')
 const helpers = require('../helpers/helpers')
 const input = fs.readFileSync('./input.txt')
                 .toString()
+                .split('\n')
+                .map(x => x.split(' '))
+                .map(x => {
+                    x[1] = Number(x[1]);
+                    return x
+                })
 
 const part1 = () => {
-    return
+    let hpos = 0;
+    let vpos = 0;
+    let aim = 0;
+
+    input.forEach(command => {
+        if (command[0] == 'forward') {
+            hpos += command[1]
+            vpos += aim * command[1]
+        } else if (command[0] == 'up') {
+            aim -= command[1]
+        } else if (command[0] == 'down') {
+            aim += command[1]
+        }
+    });
+
+    return hpos * vpos;
 }
 
 const part2 = () => {
