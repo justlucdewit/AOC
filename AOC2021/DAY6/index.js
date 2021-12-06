8e1195b7e4d2c6a7b920ffafa32f12bb7a35f726
luke <luc.dewit@hotmail.nl>
2021-12-06 13:35:16 +0100

    day 6

--- AOC2021/DAY6/index.js
+++ AOC2021/DAY6/index.js
@@ -5,43 +5,57 @@ let input = fs.readFileSync('./input.txt')
     .split(',')
     .map(Number)
 
-let fish = []
+let fish = [
+    { fishCount: 0, daysLeft: 0 },
+    { fishCount: 0, daysLeft: 1 },
+    { fishCount: 0, daysLeft: 2 },
+    { fishCount: 0, daysLeft: 3 },
+    { fishCount: 0, daysLeft: 4 },
+    { fishCount: 0, daysLeft: 5 },
+    { fishCount: 0, daysLeft: 6 },
+    { fishCount: 0, daysLeft: 7 },
+    { fishCount: 0, daysLeft: 8 }
+]
 
 input.forEach(item => {
     const match = fish.find(x => x.daysLeft == item)
-    if (match) {
-        match.fishCount++;
-    } else {
-        fish.push({
-            fishCount: 1,
-            daysLeft: item
-        })
-    }
+    match.fishCount++;
 });
 
+let fishcpy = JSON.parse(JSON.stringify(fish))
+
 const calculateNextFishGeneration = () => {
-    console.log(fish)
-
     fish.forEach(fsh => {
+        fsh.daysLeft--;
 
+        if (fsh.daysLeft < 0) {
+            const match = fish.find(x => x.daysLeft == 7)
+            match.fishCount += fsh.fishCount
+
+            fish.push({
+                fishCount: fsh.fishCount,
+                daysLeft: 8
+            })
+        }
     })
+
+    fish = fish.filter(x => x.daysLeft >= 0)
 }
 
 const part1 = () => {
-    for (let i = 0; i < 80; i++) {
+    for (let i = 0; i < 80; i++)
         calculateNextFishGeneration()
-    }
 
-    return fish.length
+    return fish.map(x => x.fishCount).sum()
 }
 
 const part2 = () => {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < 256; i++)
         calculateNextFishGeneration()
-    }
 
-    return fish.length
+    return fish.map(x => x.fishCount).sum()
 }
 
-// console.log(part1())
+console.log(part1())
+fish = fishcpy
 console.log(part2())
