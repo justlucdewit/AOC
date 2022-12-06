fdb239ff2921b846405c3db688d888d67ef70461
Luc de Wit <luc.dewit@hotmail.nl>
2022-12-06 09:51:13 +0100

    cleaned day 6

--- AOC2022/DAY6/solution.py
+++ AOC2022/DAY6/solution.py
@@ -1,44 +1,28 @@
-# Read input.txt into a string
-original_input = open("input.txt", "r").read()
+# Get the input
+inp = list(open("input.txt", "r").read())
 
-inp = list(original_input)
+def solve(n):
+    idx = 0
+    result = 0
+    while idx < len(inp):
+        # Get array of first 14 chars
+        arr = inp[idx:idx+n]
 
-def arrayIsAllUnique(arr):
-    return len(arr) == len(set(arr))
+        # If they are all different
+        if len(arr) == len(set(arr)):
+            result = idx + n
+            break
+
+        idx += 1
+    return result
 
 # 4:49
 def part1():
-    idx = 0
-    result = 0
-    while idx < len(inp):
-        # Get array of first 14 chars
-        arr = inp[idx:idx+4]
-
-        # If they are all different
-        if arrayIsAllUnique(arr):
-            result = idx + 4
-            break
-
-        idx += 1
-        
-    return result
+    return solve(4)
 
 # 1:40
 def part2():
-    idx = 0
-    result = 0
-    while idx < len(inp):
-        # Get array of first 14 chars
-        arr = inp[idx:idx+14]
-
-        # If they are all different
-        if arrayIsAllUnique(arr):
-            result = idx + 14
-            break
-
-        idx += 1
-        
-    return result
+    return solve(14)
 
 print(f"p1 = {part1()}")
 print(f"p2 = {part2()}")
